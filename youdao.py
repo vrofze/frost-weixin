e749df1342b06c290da2c4384020912199f5dd42
vrofze <vrofze@gmail.com>
2015-10-22 17:48:44 +0800

    debug

--- youdao.py
+++ youdao.py
@@ -29,9 +29,9 @@ def get(str):
         for c in dic_json["basic"]:
             res += c + ':'
             #if type(dic_json["basic"][c]) is dict:
-            for cb in c:
+            for cb in dic_json["basic"][c]:
                 res += cb + ","
-            #    res = res[:-1] + "\n"
+                res = res[:-1] + "\n"
             #else:
             #    res += dic_json["basic"][c] + "\n"
         #if dic_json["basic"]["phonetic"] != "":
