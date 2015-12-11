4cfac04ae3e8b360095a433a7f09699531d31bd7
frost <wang237321923@gmail.com>
2015-12-11 21:27:31 +0800

    add config file

--- app/main/views.py
+++ app/main/views.py
@@ -24,7 +24,7 @@ def index():
     ToUserName = xml_recv.find('ToUserName').text
     FromUserName = xml_recv.find('FromUserName').text
     querystr = xml_recv.find('Content').text.encode('utf8')
-    content = distrib(querystr)
+    content = querystr #distrib(querystr)
     reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><Funcflag>0</Funcflag></xml>'
     response = make_response(reply % (FromUserName, ToUserName, str(int(time.time())), content))
     return response
