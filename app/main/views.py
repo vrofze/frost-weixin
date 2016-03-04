3051d457f115231e74c6dfc2522bd8dcb3ac7452
vrofze <vrofze@gmail.com>
2016-03-04 21:28:29 +0800

    add uwsgi server

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,13 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 from . import main
 from flask import request, make_response
-import haslib
+import hashlib
 import xml.etree.ElementTree as ET
 import time
 
-@main.route('/', methods = ['GET', 'POST'])
+
+@main.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
         token = 'imok'
@@ -18,14 +19,25 @@ def index():
         s = [timestamp, nonce, token]
         s.sort()
         s = ''.join(s)
-        if(haslib.sha1(s).hexdigest == signature):
+        if(hashlib.sha1(s).hexdigest == signature):
             return make_response(echostr)
     xml_recv = ET.fromstring(request.data)
     ToUserName = xml_recv.find('ToUserName').text
     FromUserName = xml_recv.find('FromUserName').text
     querystr = xml_recv.find('Content').text.encode('utf8')
-    content = querystr #distrib(querystr)
-    reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><Funcflag>0</Funcflag></xml>'
-    response = make_response(reply % (FromUserName, ToUserName, str(int(time.time())), content))
+    content = querystr  # distrib(querystr)
+    reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName>'
+    '<FromUserName><![CDATA[%s]]></FromUserName>'
+    '<CreateTime>%s</CreateTime>'
+    '<MsgType><![CDATA[text]]></MsgType>'
+    '<Content><![CDATA[%s]]></Content>'
+    '<Funcflag>0</Funcflag></xml>'
+    response = make_response(
+        reply % (FromUserName, ToUserName, str(int(time.time())), content)
+    )
     return response
 
+
+@main.route('/hello', methods=['GET', 'POST'])
+def hello():
+    return 'Hello world!:p'
