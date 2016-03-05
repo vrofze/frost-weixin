0ae63cd3f24b28ee5e1b64c6a4b79964ab632a63
vrofze <vrofze@gmail.com>
2016-03-05 18:13:02 +0800

    debug[deploy]

--- manage.py
+++ manage.py
@@ -3,10 +3,11 @@
 
 import os
 from app import create_app
-from flask.ext.script import Manager, Shell
+from flask.ext.script import Server, Manager, Shell
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+# manager.add_command("runserver", Server(host='127.0.0.1', port=5000, use_debugger=True))
 
 
 def make_shell_context():
