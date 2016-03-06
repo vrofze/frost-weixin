3fda694f911cab35c2e22607f94f04479a79a777
vrofze <vrofze@gmail.com>
2016-03-06 20:34:22 +0800

    debug[deploy]

--- app/main/views.py
+++ app/main/views.py
@@ -22,18 +22,11 @@ def index():
         if(hashlib.sha1(s).hexdigest() == signature):
             return make_response(echostr)
     xml_recv = ET.fromstring(request.form['body'])
-    return make_response(request.data)
-    return make_response(xml_recv)
     ToUserName = xml_recv.find('ToUserName').text
     FromUserName = xml_recv.find('FromUserName').text
     querystr = xml_recv.find('Content').text.encode('utf8')
     content = querystr  # distrib(querystr)
-    reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName>'
-    '<FromUserName><![CDATA[%s]]></FromUserName>'
-    '<CreateTime>%s</CreateTime>'
-    '<MsgType><![CDATA[text]]></MsgType>'
-    '<Content><![CDATA[%s]]></Content>'
-    '<Funcflag>0</Funcflag></xml>'
+    reply = '<xml><ToUserName><![CDATA[%s]]></ToUserName><FromUserName><![CDATA[%s]]></FromUserName><CreateTime>%s</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[%s]]></Content><Funcflag>0</Funcflag></xml>'
     response = make_response(
         reply % (FromUserName, ToUserName, str(int(time.time())), content)
     )
