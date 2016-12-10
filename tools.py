9a5aee151034ea83f07e29aa887fe838034027f1
vrofze <vrofze@gmail.com>
2016-12-10 17:35:56 +0800

    shape code

--- tools.py
+++ tools.py
@@ -1,23 +1,23 @@
+"""tools used in main.py"""
+
 # -*- coding:utf-8 -*-
 import os
 import urllib.request
 from urllib import parse
 import json
-from setting import settings
 
-def distrib(str):
+def distrib(strl):
     """used to distribute messages"""
 
-    strt = str
-    if strt[0:3] == u"翻译:" or strt[0:3] == u"翻译:":
-        return youdao(strt[3:])
-    if strt[0:3] == u'fy ' or strt[0:3] == u'fy ':
-        return youdao(strt[3:])
+    if strl[0:3] == u"翻译:" or strl[0:3] == u"翻译:":
+        return youdao(strl[3:])
+    if strl[0:3] == u'fy ' or strl[0:3] == u'fy ':
+        return youdao(strl[3:])
     else:
-        return tuling(str)
+        return tuling(strl)
 
 
-def getHtml(url):
+def get_html(url):
     """get api"""
 
     page = urllib.request.urlopen(url)
@@ -25,25 +25,25 @@ def getHtml(url):
     return html
 
 
-def tuling(str):
+def tuling(strl):
     """tuling bot"""
 
     key = os.getenv('TULING_KEY')  # settings['tuling_key']
-    str = parse.quote(str)
-    api = "http://www.tuling123.com/openapi/api?key=%s&info=%s" % (key, str)
-    response = getHtml(api)
+    strl = parse.quote(strl)
+    api = "http://www.tuling123.com/openapi/api?key=%s&info=%s" % (key, strl)
+    response = get_html(api)
     dic_json = json.loads(response.decode('utf8'))
     return dic_json['text']
 
 
-def youdao(str):
+def youdao(strl):
     """youdao dict"""
 
-    str = parse.quote(str)
+    strl = parse.quote(strl)
     api = "http://fanyi.youdao.com/openapi.do?"\
           "keyfrom=%s&key=%s&type=data&doctype=json&version=1.1&q=%s" \
-          % (os.getenv('YOUDAO_USER'), os.getenv('YOUDAO_KEY'), str)
-    response = getHtml(api)
+          % (os.getenv('YOUDAO_USER'), os.getenv('YOUDAO_KEY'), strl)
+    response = get_html(api)
     dic = json.loads(response.decode('utf8'))
     err_dic = {
         '20': u'要翻译的文本过长',
@@ -54,19 +54,20 @@ def youdao(str):
     }
     if dic['errorCode'] != 0:
         return err_dic[dic['errorCode']]
-    res = getStr(dic)
+    res = get_str(dic)
     print(res)
     return res
 
 
-def getStr(dic):
+def get_str(dic):
+    """get str from dic"""
     res = dic['query'] + ":\n"
     res += getlist(dic['translation']) + "\n"
-    if('basic' in dic.keys()):
+    if 'basic' in dic.keys():
         res += '------基本词典------\n'
         for k, v in dic['basic'].items():
             res += transl(k) + getlist(v) + "\n"
-    if('web' in dic.keys()):
+    if 'web' in dic.keys():
         res += '------网络词典------\n'
         for c in dic['web']:
             res += getlist(c['key']) + ":"
@@ -75,52 +76,33 @@ def getStr(dic):
 
 
 def getlist(li):
+    """ get str from list"""
     res = ''
-    if(type(li) == list):
+    if isinstance(li, list):
         for v in li:
             res += getlist(v) + ','
         res = res[0:-1]
-    elif(type(li) == str):
+    elif isinstance(li, str):
         res += li
     else:
         return li
     return res
 
-'''
-def getStr(dic):
-    res = ''
-    if(type(dic) == dict):
-        for k, v in dic.items():
-            if(k == 'errorCode' or k == 'translation' or k == 'query'):
-                continue
-            res += transl(k)
-            if(v != 'key'):
-                res += getStr(v) + '\n'
-    elif(type(dic) == list):
-        for c in dic:
-            res += getStr(c) + ';'
-        res += '\n'
-    elif(type(dic) == str):
-        res += dic
-    else:
-        res += "error"
-    return res
-'''
-
-
-def transl(str):
-    dic = {
-        'basic': '-----基本词典-----\n',
-        'web': '-----网络释意-----\n',
-        'phonetic': '发音:',
-        'uk-phonetic': '英式发音:',
-        'us-phonetic': '美式发音:',
-        'explains': '解释:',
-        'key': '',
-        'value': ''
+transl_dic = {
+    'basic': '-----基本词典-----\n',
+    'web': '-----网络释意-----\n',
+    'phonetic': '发音:',
+    'uk-phonetic': '英式发音:',
+    'us-phonetic': '美式发音:',
+    'explains': '解释:',
+    'key': '',
+    'value': ''
     }
-    if(str in dic.keys()):
-        return dic[str]
+
+def transl(strl):
+    """translate english tag to chinese"""
+    if strl in transl_dic.keys():
+        return transl_dic[strl]
     return str
 
 
